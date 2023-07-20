f2952f6f82cc9d2daa551707f5b0a773bbea2a72
Amdadul Haq <amdadulhaq781@gmail.com>
2023-07-20 17:37:33 +0600

    route added

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/Home.vue'
+import { useAuthStore } from '../stores/auth'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,17 +8,36 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      meta: { title: 'Home', middleware: ['auth'] },
+      component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/register',
+      name: 'register',
+      meta: { title: 'Register', middleware: ['guest'] },
+      component: () => import('../views/auth/Register.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      meta: { title: 'Login', middleware: ['guest'] },
+      component: () => import('../views/auth/Login.vue')
     }
   ]
+})
+
+router.beforeEach(async (to, from, next) => {
+  document.title = to.meta.title + ' :: ' + import.meta.env.VITE_APP_NAME
+
+  const auth = useAuthStore()
+
+  if (!auth.isLoggedIn) {
+    await auth.fetchUser()
+  }
+
+  if (to.meta.middleware.includes('guest') && auth.isLoggedIn) next({ name: 'home' })
+  else if (to.meta.middleware.includes('auth') && !auth.isLoggedIn) next({ name: 'login' })
+  else next()
 })
 
 export default router
